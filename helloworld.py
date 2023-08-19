1e71db79494226c52261451dce698d8128f32215
bhupesh singh <109893244+bhupeshchuphal@users.noreply.github.com>
2023-08-19 05:46:11 -0700

    done some changes in the helloworld.py  Changes to be committed: 	modified:   helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,14 @@
-print("hello world")
+# write a code to print the fibonacci series till nth term
+
+n = int(input("Enter the nth term: "))
+
+a = 0
+
+b = 1
+
+for i in range(n):
+    print(a)
+    c = a + b
+    a = b
+    b = c
+    
